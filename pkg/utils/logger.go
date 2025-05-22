e544fd4892506455d4f1db41d6d68d2f16e9d5cc
swapxs <swapnil.bhowmik@xenonstack.com>
2025-05-22 15:40:50 +0530

    Feat: replace interface{} with any
    This make the code align with best practices.

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -22,21 +22,21 @@ func NewLogger(prefix string) *Logger {
 }
 
 // Info logs an info message
-func (l *Logger) Info(format string, v ...interface{}) {
+func (l *Logger) Info(format string, v ...any) {
 	l.log("INFO", format, v...)
 }
 
 // Error logs an error message
-func (l *Logger) Error(format string, v ...interface{}) {
+func (l *Logger) Error(format string, v ...any) {
 	l.log("ERROR", format, v...)
 }
 
 // Debug logs a debug message
-func (l *Logger) Debug(format string, v ...interface{}) {
+func (l *Logger) Debug(format string, v ...any) {
 	l.log("DEBUG", format, v...)
 }
 
-func (l *Logger) log(level, format string, v ...interface{}) {
+func (l *Logger) log(level, format string, v ...any) {
 	message := fmt.Sprintf(format, v...)
 	timestamp := time.Now().Format("2006-01-02 15:04:05")
 	l.logger.Printf("[%s] [%s] %s: %s", timestamp, level, l.prefix, message)
